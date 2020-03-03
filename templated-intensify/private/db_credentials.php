613db2e49c6ed29bf83cb0348ed1b428c0b22415
Damien Roenhorst <damien.roenhorst@student.graafschapcollege.nl>
2020-03-03 10:00:26 +0100

    initial commit

---
+++ templated-intensify/private/db_credentials.php
@@ -0,0 +1,7 @@
+<?php
+
+    define("DB_SERVER", "localhost");
+    define("DB_USER", "admin_new");
+    define("DB_PASS", "");
+    define("DB_NAME", "gcfestival");
+?>
---
+++ templated-intensify/private/initialize.php
@@ -0,0 +1,18 @@
+<?php
+    // Assign file paths to PHP constants
+    // __FILE__ returns the current path to this file
+    // dirname() returns the path to the parent directory
+    define("PRIVATE_PATH", dirname(__FILE__));
+    define("PROJECT_PATH", dirname(PRIVATE_PATH));
+    define("PUBLIC_PATH", PROJECT_PATH . '/public');
+    define("SHARED_PATH", PRIVATE_PATH . '/shared');
+    
+    require_once('functions.php');
+
+    define("WWW_ROOT", 'http://localhost/PHP-Opdrachten/Periode3/FestivalWebite/public/');
+
+    // require_once('db/database.php');
+
+    $db = db_connect();
+
+?>
